743770c57a51a1928b3f48568297cba4dacc8a01
todstoychev <todstoychev@gmail.com>
2016-11-11 12:00:51 +0200

    Initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+service supervisor start
+
+set -e
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+    set -- redis-server "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+    exec redis-server "$0" "$@"
+fi
+
+tail -f /dev/null
+exec "$@"
